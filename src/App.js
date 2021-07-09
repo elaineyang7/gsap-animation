5826ac72b145c0e16d0939788ef5fe390aa09ff6
elaineyang7 <elaineyyang7@gmail.com>
2021-07-09 11:32:45 -0700

    add animations using GSAP

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
+import {TweenMax, TimelineLite, Power3} from 'gsap';
 
 import './App.scss';
 
@@ -6,14 +7,50 @@ import meow1 from "./images/meow2.jpg";
 import meow2 from "./images/meow1.jpg";
 import arrow from './images/arrow-right.svg'
 
-
 function App() {
+
+  let app = useRef(null);
+  let content = useRef(null);
+  let images = useRef(null);
+
+  let tl = new TimelineLite();
+
+    useEffect(() => {
+      const headlineFirst = content.children[0].children[0];
+      const headlineSecond = headlineFirst.nextSibling;
+      const contentP = content.children[1];
+      const contentButton = content.children[2];
+
+      const meow1Image = images.firstElementChild;
+      const meow2Image = images.lastElementChild;
+
+      TweenMax.to(app, 0, {css: {visibility: 'visible'}})
+
+      
+      
+      tl.from(meow1Image, 1.5, {y: 1280, ease: Power3.easeInOut})
+        .from(meow1Image.firstElementChild, 2, {scale: 2, ease: Power3.easeOut}, 0.2)
+        .from(meow2Image, 1.4, {y: 1280, ease: Power3.easeOut}, 0.2)
+        .from(meow2Image.firstElementChild, 2, {scale: 1.6, ease: Power3.easeOut}, 0.2)
+      
+
+      tl.staggerFrom([headlineFirst.children, headlineSecond.children ], 1, {
+        y: 44,
+        ease:Power3.easeOut,
+        delay: .8
+      }, .15, 'Start')
+        .from(contentP, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, 1.4)
+        .from(contentButton, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, 1.6)
+    
+    }, [tl])
+  
+
   return (
-    <div className="hero">
+    <div className="hero" ref={el => app = el}>
       <div className="container">
         <div className="hero-inner">
           <div className="hero-content">
-            <div className="hero-content-inner">
+            <div className="hero-content-inner" ref={el => content = el}>
               <h1>
                 <div className="hero-content-line">
                   <div className="hero-content-line-inner">Our Love</div>
@@ -36,7 +73,7 @@ function App() {
             </div>
           </div>
           <div className="hero-images">
-            <div className="hero-images-inner">
+            <div className="hero-images-inner" ref={el => images = el}>
               <div className="hero-image meow1">
                 <img src={meow1} alt="meow1" />
               </div>
